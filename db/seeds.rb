f9868dc81fc5ad93473341cf1f99fce4a30d2d08
Adriana Shibata <dri_src@hotmail.com>
2023-11-15 17:10:18 +0100

    yelp-review could be better

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+giorgia = {name: "Giorgia Trattoria", address: "Weißenburger Str. 2, 81667 München", phone_number: "089 594393", category: "italian"}
+eataly = {name: "Eataly", address: "Blumenstraße 4, 80331 München", phone_number: "089 21994870", category: "italian"}
+yuki_hana = {name: "Yuki Hana", address: "Stollbergstraße 6, 80539 München", phone_number: "089 21269018", category: "japanese"}
+mai_garten = {name: "Mai Garten", address: "Ohlmüllerstraße 24, 81541 München", phone_number: "089 62423888", category: "chinese"}
+le_faubourg = {name: "Le Faubourg", address: "Kirchenstraße 5, 81675 München", phone_number: "089 475533", category: "french"}
+
+[giorgia, eataly, yuki_hana, mai_garten, le_faubourg].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
